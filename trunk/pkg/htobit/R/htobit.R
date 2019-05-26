acaa283da6bde35a3010621678d8972660bb53a8
azeileis <azeileis@630eee50-358b-4df4-8019-841fff668cad>
2019-05-26 22:55:58 +0000

    day 2a: added a htobit class and some basic S3 methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uibk-rprog-2019@3 630eee50-358b-4df4-8019-841fff668cad

--- trunk/pkg/htobit/R/htobit.R
+++ trunk/pkg/htobit/R/htobit.R
@@ -32,9 +32,44 @@ htobit <- function(x, y, z = x, start = NULL, ...)
   ## optimization
   opt <- optim(par = start, fn = nll, ...)
 
-  ## enhance labels
+  ## collect information
   names(opt)[1:2] <- c("coefficients", "loglik")
+  opt$coefficients <- list(
+    location = opt$coefficients[1:m],
+    scale = opt$coefficients[m + 1:p]
+  )
+  names(opt$coefficients$location) <- colnames(x)
+  names(opt$coefficients$scale) <- colnames(z)
   opt$loglik <- -opt$loglik
+  opt$nobs <- n
+  opt$df <- m + p
+  class(opt) <- "htobit"
   
   return(opt)
+}
+
+logLik.htobit <- function(object, ...) {
+  structure(object$loglik, df = object$df, class = "logLik")
+}
+
+coef.htobit <- function(object, model = c("full", "location", "scale"), ...) {
+  model <- match.arg(model)
+  cf <- object$coefficients
+  switch(model,
+    "location" = cf$location,
+    "scale" = cf$scale,
+    "full" = c(cf$location, cf$scale),
+  )
+}
+
+print.htobit <- function(x, digits = max(3, getOption("digits") - 3), ...)
+{
+  cat("Heteroscedastic tobit model\n\n")
+  cat("Coefficients (location model):\n")
+  print.default(format(x$coefficients$location, digits = digits), print.gap = 2, quote = FALSE)
+  cat("\nCoefficients (scale model with log link):\n")
+  print.default(format(x$coefficients$scale, digits = digits), print.gap = 2, quote = FALSE)
+  cat(sprintf("\nLog-likelihood: %s on %s Df\n", format(x$loglik, digits = digits), x$df))
+
+  invisible(x)
 }
