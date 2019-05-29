cbff4761ad6fd2940e275ad138aa66c87a81e9dc
azeileis <azeileis@630eee50-358b-4df4-8019-841fff668cad>
2019-05-29 00:40:16 +0000

    day 2b: added a Formula-based interface with more S3 methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uibk-rprog-2019@18 630eee50-358b-4df4-8019-841fff668cad

--- pkg/htobit/R/htobit.R
+++ pkg/htobit/R/htobit.R
@@ -1,8 +1,78 @@
-htobit <- function(x, y, z = x, start = NULL, ...)
+htobit <- function(formula, data, subset, na.action,
+  model = TRUE, y = TRUE, x = FALSE,
+  control = htobit_control(...), ...)
+{
+  ## call
+  cl <- match.call()
+  if(missing(data)) data <- environment(formula)
+  mf <- match.call(expand.dots = FALSE)
+  m <- match(c("formula", "data", "subset", "na.action"), names(mf), 0L)
+  mf <- mf[c(1L, m)]
+  mf$drop.unused.levels <- TRUE
+
+  ## formula
+  oformula <- as.formula(formula)
+  formula <- as.Formula(formula)
+  if(length(formula)[2L] < 2L) {
+    formula <- as.Formula(formula(formula), ~ 1)
+  } else {
+    if(length(formula)[2L] > 2L) {
+      formula <- Formula(formula(formula, rhs = 1L:2L))
+      warning("formula must not have more than two RHS parts")
+    }
+  }
+  mf$formula <- formula
+
+  ## evaluate model.frame
+  mf[[1L]] <- as.name("model.frame")
+  mf <- eval(mf, parent.frame())
+
+  ## extract terms, model matrix, response
+  mt <- terms(formula, data = data)
+  mtX <- terms(formula, data = data, rhs = 1L)
+  mtZ <- delete.response(terms(formula, data = data, rhs = 2L))
+  Y <- model.response(mf, "numeric")
+  X <- model.matrix(mtX, mf)
+  Z <- model.matrix(mtZ, mf)
+
+  ## sanity check
+  if(length(Y) < 1) stop("empty model")
+  n <- length(Y)
+
+  ## call the actual workhorse: htobit_fit()
+  rval <- htobit_fit(X, Y, Z, control)
+
+  ## further model information
+  rval$call <- cl
+  rval$formula <- oformula
+  rval$terms <- list(location = mtX, scale = mtZ, full = mt)
+  rval$levels <- list(location = .getXlevels(mtX, mf), scale = .getXlevels(mtZ, mf), full = .getXlevels(mt, mf))
+  rval$contrasts <- list(location = attr(X, "contrasts"), scale = attr(Z, "contrasts"))
+  if(model) rval$model <- mf
+  if(y) rval$y <- Y
+  if(x) rval$x <- list(location = X, scale = Z)
+  class(rval) <- "htobit"
+  return(rval)
+}
+
+htobit_control <- function(maxit = 5000, start = NULL, ...)
+{
+  ctrl <- c(
+    list(maxit = maxit,
+    start = start), list(...)
+  )
+  if(!is.null(ctrl$fnscale)) warning("fnscale must not be modified")
+  ctrl$fnscale <- 1
+  if(is.null(ctrl$reltol)) ctrl$reltol <- .Machine$double.eps^(1/1.2)
+  ctrl
+}
+
+htobit_fit <- function(x, y, z = NULL, control)
 {
   ## dimensions
   n <- length(y)
-  m <- ncol(x)
+  if(is.null(z)) matrix(1, n, 1, dimnames = list(rownames(x), "(Intercept)"))
+  m <- ncol(x)  
   p <- ncol(z)
   stopifnot(n == nrow(x), n == nrow(z))
 
@@ -21,16 +91,18 @@ htobit <- function(x, y, z = x, start = NULL, ...)
   }
   
   ## starting values (by default via OLS)
-  if(is.null(start)) {
+  if(is.null(control$start)) {
     start <- lm.fit(x, y)
     start <- c(start$coefficients,
       log(mean(start$residuals^2))/2, rep.int(0, p - 1))
   } else {
+    start <- control$start
     stopifnot(length(start) == m + p)
   }
+  control$start <- NULL
   
   ## optimization
-  opt <- optim(par = start, fn = nll, ...)
+  opt <- optim(par = start, fn = nll, control = control)
 
   ## collect information
   names(opt)[1:2] <- c("coefficients", "loglik")
@@ -43,7 +115,6 @@ htobit <- function(x, y, z = x, start = NULL, ...)
   opt$loglik <- -opt$loglik
   opt$nobs <- n
   opt$df <- m + p
-  class(opt) <- "htobit"
   
   return(opt)
 }
@@ -64,7 +135,7 @@ coef.htobit <- function(object, model = c("full", "location", "scale"), ...) {
 
 print.htobit <- function(x, digits = max(3, getOption("digits") - 3), ...)
 {
-  cat("Heteroscedastic tobit model\n\n")
+  cat("\nCall:", deparse(x$call, width.cutoff = floor(getOption("width") * 0.85)), "", sep = "\n")
   cat("Coefficients (location model):\n")
   print.default(format(x$coefficients$location, digits = digits), print.gap = 2, quote = FALSE)
   cat("\nCoefficients (scale model with log link):\n")
@@ -72,4 +143,20 @@ print.htobit <- function(x, digits = max(3, getOption("digits") - 3), ...)
   cat(sprintf("\nLog-likelihood: %s on %s Df\n", format(x$loglik, digits = digits), x$df))
 
   invisible(x)
+}
+
+terms.htobit <- function(x, model = c("location", "scale", "full"), ...) x$terms[[match.arg(model)]]
+
+model.frame.htobit <- function(formula, ...) {
+  if(!is.null(formula$model)) return(formula$model)
+  formula$terms <- formula$terms$full
+  formula$call$formula <- formula$formula <- formula(formula$terms)
+  NextMethod()
+} 
+
+model.matrix.htobit <- function(object, model = c("location", "scale"), ...) {
+  model <- match.arg(model)
+  rval <- if(!is.null(object$x[[model]])) object$x[[model]]
+    else model.matrix(object$terms[[model]], model.frame(object), contrasts = object$contrasts[[model]])
+  return(rval)
 }
