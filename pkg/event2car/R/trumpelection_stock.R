5c28f36127e2d70511495ddb641b0deb07f4ea2c
lisalechner <lisalechner@630eee50-358b-4df4-8019-841fff668cad>
2019-06-19 07:21:32 +0000

    modified data.R skript, where content is NULL
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uibk-rprog-2019@62 630eee50-358b-4df4-8019-841fff668cad

--- pkg/event2car/R/trumpelection_stock.R
+++ pkg/event2car/R/trumpelection_stock.R
@@ -5,7 +5,13 @@
 #' The dataset covers 356 days before the election (e.g. 2015-11-09)
 #' and 365 days after the election (e.g. 2017-11-08). This results in 503 trading days.
 #'
-#' @format Two zoo objects and a date object.
+#' @docType data
+#'
+#' @usage data(trumpelection_stock)
+#'
+#' @keywords datasets
+#'
+#' @format Two objects of class \code{zoo} and one object of class \code{Date}.
 #' \describe{
 #'   \item{returns_firms}{stock return of 18 US tech firms from 2015-11-09 to 2017-11-08.}
 #'   \item{return_indx}{NASDAQ return  from 2015-11-09 to 2017-11-08.}
@@ -13,4 +19,16 @@
 #'   ...
 #' }
 #' @source \url{https://finance.yahoo.com/}
-"trumpelection_stock"
+#'
+#' #' @examples
+#' # load data
+#' data('trumpelection_stock')
+#' # mean adjusted model
+#' event2car(returns=returns_firms,regressor=return_indx,event_dates =trumpelection,market_model="mean_adj" )
+#' # market adjusted model (out-of sample estimation)
+#' event2car(returns=returns_firms,regressor=return_indx,event_dates =trumpelection,market_model="mrkt_adj" )
+#' # market adjusted model (within sample estimation)
+#' event2car(returns=returns_firms,regressor=return_indx,event_dates =trumpelection,market_model="mrkt_adj_within" )
+
+NULL
+
