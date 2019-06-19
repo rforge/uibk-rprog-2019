d3857ef88ca1e3a18ea2cc9b940745a72b89b522
lisalechner <lisalechner@630eee50-358b-4df4-8019-841fff668cad>
2019-06-19 05:11:00 +0000

    add data description file to R.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uibk-rprog-2019@59 630eee50-358b-4df4-8019-841fff668cad

---
+++ pkg/event2car/R/trumpelection_stock.R
@@ -0,0 +1,16 @@
+#' The Trump election and stock returns of 18 US tech firms.
+#'
+#' Datset containing stock returns of 18 important US tech firms (See https://finance.yahoo.com/u/yahoo-finance/watchlists/tech-stocks-that-move-the-market/),
+#' the NASDAQ return and the election date of president Trump.
+#' The dataset covers 356 days before the election (e.g. 2015-11-09)
+#' and 365 days after the election (e.g. 2017-11-08). This results in 503 trading days.
+#'
+#' @format Two zoo objects and a date object.
+#' \describe{
+#'   \item{returns_firms}{stock return of 18 US tech firms from 2015-11-09 to 2017-11-08.}
+#'   \item{return_indx}{NASDAQ return  from 2015-11-09 to 2017-11-08.}
+#'   \item{trumpelection}{Date of Donald Trump Election.}
+#'   ...
+#' }
+#' @source \url{https://finance.yahoo.com/}
+"trumpelection_stock"
