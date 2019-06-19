52eff6b85e4d5538ba76ad34d5a44fe3451aaab1
lisalechner <lisalechner@630eee50-358b-4df4-8019-841fff668cad>
2019-06-19 08:25:40 +0000

    add example in event2car.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uibk-rprog-2019@68 630eee50-358b-4df4-8019-841fff668cad

--- pkg/event2car/R/event2car.R
+++ pkg/event2car/R/event2car.R
@@ -41,6 +41,22 @@
 #' European Economic Review, 110:1-17, 2018.
 #' @importFrom plyr ldply
 #' @importFrom zoo index merge.zoo coredata
+#' 
+#' @examples
+#' data('tech_returns')
+# # prepare data
+#' trumpelection <- as.Date("2016-11-08")
+#' returns_firms=tech_returns[,2:19]
+#' return_indx = tech_returns[,1]
+#' # mean adjusted model
+#' event2car(returns=returns_firms,regressor=return_indx,
+#'           event_dates=trumpelection,market_model="mean_adj")
+#' # market adjusted model (out-of sample estimation)
+#' event2car(returns=returns_firms,regressor=return_indx,
+#'           event_dates=trumpelection,market_model="mrkt_adj")
+#' # market adjusted model (within sample estimation)
+#' event2car(returns=returns_firms,regressor=return_indx,
+#'           event_dates=trumpelection,market_model="mrkt_adj_within")
 
 
 #' @export
