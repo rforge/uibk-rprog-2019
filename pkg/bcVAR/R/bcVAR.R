2342fd31f5966641b7e45f925f60bed8ac1d9698
sroeck <sroeck@630eee50-358b-4df4-8019-841fff668cad>
2019-07-17 18:59:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uibk-rprog-2019@94 630eee50-358b-4df4-8019-841fff668cad

--- pkg/bcVAR/R/bcVAR.R
+++ pkg/bcVAR/R/bcVAR.R
@@ -12,7 +12,7 @@
 
   Y <- y[, p:T]
   
-  if (p < 2){
+  if (p < 2) {
     Y <- Y
   } else {
     for (i in 1:(p-1)){
@@ -26,6 +26,19 @@
     X <- rbind(matrix(1, 1, T-p), Y[, 1:(T-p)])
   }
   Y <- Y[, 2:(T-p+1)]
+  
+  ## for datamat (Pfaff)
+  p1 = p + 1
+  datamat <- y[, p1:T]
+  for (i in 1:(p1-1)){
+    datamat <- rbind(datamat, y[, (p1-i):(T-i)])
+  } 
+  if (constant == FALSE){
+    datamat <- datamat
+  } else {
+    datamat <- rbind(datamat, matrix(1, 1, T-p))
+    rownames(datamat)[K*p1+1] <- "const"
+  }
   
   ## OLS (reduced-form)
   Ahat <- lm.fit(t(X), t(Y))$coefficients
@@ -41,6 +54,7 @@
 
   ## estiamted reduced-form residuals
   Uhat <- Y - Ahat%*%X
+  residuals <- t(Uhat[1:K, ])
   
   ## cov-var matrix reduced-form residuals
   SIGMA <- Uhat%*%t(Uhat)*(T-p-K*p-1)^(-1)
@@ -51,7 +65,7 @@
   SIGMAU <- Uhat%*%t(Uhat)*(T-p-K*p-1)^(-1)
   I <- diag(K*p)
   
-  vecSIGMAY <- solve(diag((K*p)^2) - kronecker(A.companion.hat, A.companion.hat)) %*% matrixcalc::vec(SIGMAU)
+  vecSIGMAY <- solve(diag((K*p)^2) - kronecker(A.companion.hat, A.companion.hat)) %*% vec(SIGMAU)
   dim(vecSIGMAY) <- c(K*p, K*p)          
   GAMMAY <- vecSIGMAY
   
@@ -68,27 +82,42 @@
   correction <- BA/(T-p)
   bcLS <- A.companion.hat + correction
   
+  if (constant == FALSE){
+    coeff <- bcLS
+  } else {
+    coeff <- cbind(bcLS, V)
+  }
+  
+  ## save the results like the vars package (Pfaff)
+  varresult <- list()
+  for (i in 1:K){
+    varresult[[i]] <- list()
+    class(varresult[[i]]) <- "lm"
+    varresult[[i]]$coefficients <- coeff[i, ]
+    varresult[[i]]$residuals <- residuals[, i]
+  }
+  names(varresult) <- c(names(data))
+  
+  call <- match.call()
+  
   ## return everything
   result <- list(
-    "V" = if (constant == FALSE){
-      "V" = c("no constant")
-    } else {
-      "V" = V
-    },
-    "A" = unname(A.companion.hat),
-    "bcLS" = unname(bcLS),
-    "SIGMA" = SIGMA,
-    "Y" = Y,
-    "Uhat" = Uhat,
-    "sumstat" = c(
-      "T" = T,
-      "K" = K,
-      "p" = p
-    )
+    varresult = varresult,
+    datamat = t(datamat), 
+    y = data,
+    p = p, 
+    K = K,
+    obs = T,
+    toobs = T - p,
+    restrictions = NULL,
+    call = call
   )
+  
   class(result) <- "varest"
   return(result)
+  
 }
+
 
 
 
